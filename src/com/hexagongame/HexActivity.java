5b2aa0c899bf4d4d0b93790ee9b798f6aed85a69
lcrudd <lcrudd@googlemail.com>
2013-01-03 14:33:19 +0000

    removed cumbersome "decorate board" functions, added background bitmaps

--- src/com/hexagongame/HexActivity.java
+++ src/com/hexagongame/HexActivity.java
@@ -1,10 +1,9 @@
 package com.hexagongame;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class HexActivity extends Activity {
 
@@ -26,8 +25,8 @@ public class HexActivity extends Activity {
     
     private void initializeLayout()
     {
-    	setContentView(R.layout.main);
-    	
+    	setContentView(R.layout.main);	
+
     	String gameModeStr = getIntent().getStringExtra(ChooseBoardActivity.ID_GAME_MODE);
         int gameMode = (gameModeStr != null) ? Integer.valueOf(gameModeStr) : 0;
         
@@ -37,9 +36,20 @@ public class HexActivity extends Activity {
         String boardShapeStr = getIntent().getStringExtra(ChooseBoardActivity.ID_BOARD_VIEW);
         int boardShape = (boardShapeStr != null) ? Integer.valueOf(boardShapeStr) : 0;
         
-        UiView uiView = (UiView) findViewById(R.id.boardview);       
+        UiView uiView = (UiView) findViewById(R.id.boardview);
+
         uiView.gameMode = gameMode;
         uiView.phonePlayerId = phonePlayerId;
         uiView.boardShape = boardShape;
+        
+        if (boardShape == Board.BOARD_GEOMETRY_RECT)
+        {
+        	//rectangle
+        	uiView.setBackgroundResource(R.drawable.background_sq);
+        } else
+        {
+        	//hexagon
+        	uiView.setBackgroundResource(R.drawable.background);
+        }
     }
 }
