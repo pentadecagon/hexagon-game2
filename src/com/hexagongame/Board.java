610fcd1a69edb91d972049dfe9f8567d8021cfd3
pentadecagon <pentadecagon@gmail.com>
2013-01-06 11:29:34 +0000

    Add some artificial intelligence to the phone player

--- src/com/hexagongame/Board.java
+++ src/com/hexagongame/Board.java
@@ -1,8 +1,8 @@
 package com.hexagongame;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Set;
 
 import android.util.Log;
 
@@ -349,14 +349,14 @@ public class Board{
 		return wCell;
 	}
 	
-	static void addToSet( HashSet<Hexagon> s, Hexagon h ){
+	static void addToSetSameColor( HashSet<Hexagon> s, Hexagon h ){
 		if( s.contains(h))
 			return;
 		
 		s.add(h);
 		for( Hexagon u : h.adjacent ){
 			if( u.color == h.color )
-				addToSet( s, u );
+				addToSetSameColor( s, u );
 		}
 	}
 	
@@ -364,8 +364,8 @@ public class Board{
 	{
 		HashSet<Hexagon> s1 = new HashSet<Hexagon>();
 		HashSet<Hexagon> s2 = new HashSet<Hexagon>();
-		addToSet( s1, outer[p][0] );
-		addToSet( s2, outer[p][1] );
+		addToSetSameColor( s1, outer[p][0] );
+		addToSetSameColor( s2, outer[p][1] );
 		s1.retainAll(s2 );
 		if( s1.size() > 0 ){
 			Log.i("hex", "WINNER!");
@@ -374,6 +374,95 @@ public class Board{
 			return false;
 		}
 	}
+	
+	static HashSet<Hexagon> allNeighbors( Hexagon a, int color )
+	{
+		HashSet<Hexagon> scol = new HashSet<Hexagon>();
+		for( Hexagon hex : a.adjacent ){
+			if( hex.color == color ){
+				addToSetSameColor(scol, hex );
+			}
+		}
+		scol.add(a);
 
+		HashSet<Hexagon> erg = new HashSet<Hexagon>();
+		for( Hexagon hex : scol ){
+			for( Hexagon u : hex.adjacent ){
+				if( u.color == UiView.HEX_UNUSED_COLOR ){
+					erg.add(u);
+				}
+			}
+		}
+		return erg;
+	}
+			
+	static HashMap<Hexagon, Double>	pathValue( HashSet<Hexagon> a, HashSet<Hexagon> a_opp, int color ){
+		HashMap<Hexagon, Double> erg = new HashMap<Hexagon, Double>();
+		HashSet<Hexagon> lastlevel = new HashSet<Hexagon>();
+		for( Hexagon hex : a ){
+			erg.put(hex,  1.0 );
+			lastlevel.add(hex);
+		}
+		while (lastlevel.size() > 0){
+			HashMap<Hexagon, Double> erg2 = new HashMap<Hexagon, Double>();
+			HashSet<Hexagon> nextlevel = new HashSet<Hexagon>();
+			lastlevel.removeAll(a_opp);
+			for( Hexagon hex : lastlevel ){
+				double newval = erg.get(hex) / 4.0;
+				for( Hexagon next : allNeighbors(hex, color )){
+					if( erg.containsKey(next))
+						continue;
+				
+					if( erg2.containsKey(next))
+						erg2.put( next, erg2.get(next)+newval );
+					else
+						erg2.put( next,  newval);
+					nextlevel.add(next);
+				}
+				erg.putAll(erg2);
+				lastlevel = nextlevel;
+			}
+		}		
+		return erg;
+	}
+	
+	HashMap<Hexagon, Double> analyze( int p ){
+		final HashSet<Hexagon> swhite[] = new HashSet[2];
+		final int thiscolor = outer[p][0].color;
+		for( int i=0; i<2; ++i ){
+			final HashSet<Hexagon> sblue= new HashSet<Hexagon>();
+			addToSetSameColor( sblue, outer[p][i] );
+			swhite[i]= new HashSet<Hexagon>();
+			for( Hexagon hb : sblue ){
+				for( Hexagon hex: hb.adjacent ){
+					if( hex.color == UiView.HEX_UNUSED_COLOR )
+						swhite[i].add(hex);
+				}
+			}
+		}
+		HashMap<Hexagon, Double> v1 = pathValue( swhite[0], swhite[1], thiscolor ); 
+		HashMap<Hexagon, Double> v2 = pathValue( swhite[1], swhite[0], thiscolor );
+		HashMap<Hexagon, Double> erg = new HashMap<Hexagon, Double>();		
+		for( Hexagon hex : v1.keySet() ) if( v2.containsKey(hex)){
+			double val = v1.get(hex) * v2.get(hex);
+			erg.put( hex,  val );
+		}
+		return erg;
+	} // end analyze
 
+	Hexagon analyzeAll(){
+		HashMap<Hexagon, Double> v1 = analyze(0);
+		HashMap<Hexagon, Double> v2 = analyze(1);
+		Hexagon besthex = null;
+		double bestval = 0;
+		for( Hexagon hex : v1.keySet() ) if( v2.containsKey(hex)){
+			double val = v1.get(hex) + v2.get(hex);
+			if( val > bestval ){
+				bestval = val;
+				besthex = hex;
+			}
+		}
+		return besthex;
+	}
 }
+
