f4ed7de005e7c5d0f1b766e9e06598fd5b8ac2ef
lcrudd@googlemail.com <lcrudd@googlemail.com@e0b0e452-e103-5d90-52a1-fccd39ac0037>
2013-01-06 15:26:49 +0000

    added winner screen

--- trunk/src/com/hexagongame/ChooseBoardActivity.java
+++ trunk/src/com/hexagongame/ChooseBoardActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
-import android.widget.Toast;
 
 public class ChooseBoardActivity extends Activity {
 	
