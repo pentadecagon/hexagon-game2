fb822b632795251b35e7d30b16808385960b7521
pentadecagon <pentadecagon@gmail.com>
2013-01-06 07:21:54 +0000

    Currently we use a separate value, "touches_edge", for keeping track of hexagons adjacent to the four outer regions of the board.  It's much simpler to use separate "Hexagon" objects for that purpose, because then we have a generic relation "adjacent" that works for both hexagons and outer regions.

--- src/com/hexagongame/Hexagon.java
+++ src/com/hexagongame/Hexagon.java
@@ -9,15 +9,12 @@ public class Hexagon {
 	
 	final public float x, y;
 	
-	final public boolean[] touches_edge;
-	
 	final ArrayList<Hexagon> adjacent = new ArrayList<Hexagon>();
 	
-	public Hexagon(float x, float y, int color, boolean[] touches_edge)
+	public Hexagon(float x, float y, int color)
 	{
 		this.x = x;
 		this.y = y;
 		this.color = color;
-		this.touches_edge = touches_edge;
 	}
 }
