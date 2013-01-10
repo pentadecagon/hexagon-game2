b6360b06317cacbbb8b2d31d3779b23ec72cbcbd
pentadecagon@gmail.com <pentadecagon@gmail.com@e0b0e452-e103-5d90-52a1-fccd39ac0037>
2013-01-10 14:28:28 +0000

    simnplify DrawBoardHelper.  Now the layout algorithm is generic and does not depend on "Rectangle" vs. "Hexagon"-shaped board, it now should work any shape and size.

--- trunk/src/com/hexagongame/DrawBoardHelper.java
+++ trunk/src/com/hexagongame/DrawBoardHelper.java
@@ -6,15 +6,6 @@ public class DrawBoardHelper {
 
 	public Board board = null;
 	
-	//width of canvas
-	private static float canvasWidth;
-
-	//height of canvas
-	private static float canvasHeight;
-		
-	//width of board
-	private static float boardWidth;
-
 	//x0, x-position of the top-left corner of the hexagon relative to the canvas
 	private static float x0;
 	
@@ -25,68 +16,32 @@ public class DrawBoardHelper {
 	
 	//height of the rectangular grid cell
 	private static float hCell;
-	
 	//width of the rectangular grid cell
 	private static float wCell;
 	
 	public DrawBoardHelper(float canvasHeight, float canvasWidth, Board board)
 	{
-		DrawBoardHelper.canvasWidth = canvasWidth;
-		DrawBoardHelper.canvasHeight = canvasHeight;
 		this.board = board;
-		
-		initialize();
-	}
-	
-
-	private void initialize()
-	{
-		//calculate width of board
-		boardWidth = 0.8f * canvasWidth;
-		
-		Log.d("hex","calculated boardWidth="+boardWidth);
-
-		//calculate the length of the side of one of the small hexagons that fills up the board
-		calculateSmallHexagonSideLength();
-		
-		//calculate height and width of one grid cell
-		calculateDimensionsOfGridCell();
-		
-		//calculate (x0, y0) position of the board (position of the top-left point of the hexagon)
-		//relative to the canvas
-		calculatePositionOfBoardOnCanvas();
-	}
-	
-
-	private void calculateSmallHexagonSideLength()
-	{
-		smallHexSideLength = boardWidth/ (14.0f * (float) Math.cos(Math.PI/6.0));
-		Log.d("hex","smallHexSideLength: "+smallHexSideLength);
-	}
-	
-	private void calculateDimensionsOfGridCell()
-	{
 		wCell = 2.0f * smallHexSideLength * (float) Math.cos((Math.PI/ 6.0));
-		hCell = smallHexSideLength * (1.0f + (float) Math.cos((Math.PI/ 3.0)));
-	}
-	
-	
-	private void calculatePositionOfBoardOnCanvas()
-	{
-		
-		x0 = 0.1f * canvasWidth;
-		
-		float boardHeight;
-		if (this.board.boardShape == Board.BOARD_GEOMETRY_RECT)
-		{
-			boardHeight = hCell * 7.0f - smallHexSideLength * (float) Math.sin((Math.PI/ 6.0));
-		} else
-		{
-			boardHeight = hCell * 9.0f - smallHexSideLength * (float) Math.sin((Math.PI/ 6.0));
+		hCell = smallHexSideLength * 1.5f;
+		int imax=-10000;
+		int jmax=-10000;
+		int jmin=10000;
+		for( Hexagon hex : board.hexagonList ){
+			imax = Math.max(imax, hex.i );
+			jmax = Math.max(jmax,  hex.j );
+			jmin = Math.min(jmin,  hex.j );
 		}
 		
-		y0 = canvasHeight/2.0f - boardHeight/2.0f;	
-				
+		wCell = canvasWidth / (imax+2);
+		smallHexSideLength = wCell / (float)Math.sqrt(3.0);
+		hCell = smallHexSideLength * 1.5f;
+		
+		x0 = wCell * 0.5f;
+		
+		float jmid = (jmax+jmin) * 0.5f; // this should be placed at canvasHeight/2
+		
+		y0 = canvasHeight * 0.5f - jmid * hCell + 0.25f * smallHexSideLength;
 		Log.d("hex","calculated position of board on canvas: x0="+x0+", y0="+y0);
 	}
 	
@@ -94,22 +49,11 @@ public class DrawBoardHelper {
 	public float[] findPositionOfCenterOfHexagonalCell(int hexGridCoordX, int hexGridCoordY)
 	{
 		//find the position of the top-left of the hexagon relative to the (0, 0) position of the board
-		float yRel = hCell * (-(float) hexGridCoordY - 0.5f);
-		float xRel;
-		//check if this is an even or odd row in the grid
-		if (hexGridCoordY % 2 == 0)
-		{
-			xRel = wCell * (float) hexGridCoordX;
-		} else
-		{
-			xRel = wCell * ((float) hexGridCoordX + 0.5f);
-		}
-		
-		float x = xRel + getXpositionOfBoardOnCanvas();
-		float y = yRel + getYpositionOfBoardOnCanvas();
-		
+		float x = x0 + wCell * (hexGridCoordX + (Math.abs(hexGridCoordY) % 2) * 0.5f);
+		float y = y0 + hCell * hexGridCoordY;
+
 		float[] hexCellPos = {x, y};
-		return hexCellPos;
+		return hexCellPos; 
 	}
 	
 	
@@ -130,18 +74,7 @@ public class DrawBoardHelper {
 	    }
 	    return besthex;
 	}
-	
-
-	public float getXpositionOfBoardOnCanvas()
-	{
-		return x0;
-	}
-	
-	public float getYpositionOfBoardOnCanvas()
-	{
-		return y0;
-	}
-	
+		
 	public float getSmallHexSideLength()
 	{
 		return smallHexSideLength;
@@ -155,15 +88,5 @@ public class DrawBoardHelper {
 	public float getWCell()
 	{
 		return wCell;
-	}
-	
-	public float getCanvasWidth()
-	{
-		return canvasWidth;
-	}
-	
-	public float getCanvasHeight()
-	{
-		return canvasHeight;
 	}
 }
