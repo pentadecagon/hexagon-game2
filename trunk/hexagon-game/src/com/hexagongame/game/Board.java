340c094ec9c2e36b185193b39ff2fbf26d3d1767
pentadecagon@gmail.com <pentadecagon@gmail.com@e0b0e452-e103-5d90-52a1-fccd39ac0037>
2013-02-03 12:42:21 +0000

    Fix crash with large or small boards

--- trunk/hexagon-game/src/com/hexagongame/game/Board.java
+++ trunk/hexagon-game/src/com/hexagongame/game/Board.java
@@ -3,6 +3,8 @@ package com.hexagongame.game;
 import java.util.ArrayList;
 import java.util.HashSet;
 
+import android.util.Log;
+
 import com.hexagongame.game.Hexagon.HexSet;
 
 //import android.util.Log;
@@ -167,18 +169,18 @@ public class Board{
 				Hexagon hex = new Hexagon( i+k*0.5f, k, Hexagon.OWNER_EMPTY, id++ );
 				if( Math.abs(i) == r || Math.abs(k) == r || Math.abs( i+k ) == r ){ // its at the edge
 					final int x = 2*i+k; 
-					if( x>=1 && k>=0 )
+					if( x>=boardSize && k>=0 )
 						outer[0][0].adjacent.add(hex);
-					if( x>=-1 && k<=0 )
+					if( x>=-boardSize && k<=0 )
 						outer[1][0].adjacent.add(hex);
-					if( x<=1 && k>=0 )
+					if( x<=boardSize && k>=0 )
 						outer[1][1].adjacent.add(hex);
-					if( x<=-1 && k<=0 )
+					if( x<=-boardSize && k<=0 )
 						outer[0][1].adjacent.add(hex);
 				}
 		        hexagonList.add(hex);
 			}
-//		Log.i("hex", "out size: 0: " + outer[0][0].adjacent.size()+" "+outer[0][1].adjacent.size() + " 1: " + outer[1][0].adjacent.size() + " " + outer[1][1].adjacent.size() );
+		Log.i("hex", "out size: 0: " + outer[0][0].adjacent.size()+" "+outer[0][1].adjacent.size() + " 1: " + outer[1][0].adjacent.size() + " " + outer[1][1].adjacent.size() );
 	}
 	
 	private void setupRectBoardListOfHexagons()
